e41857ab72ed559ea56affa5da247842bd3c865c
Ludens1111 <Leekyehun@gmail.com>
2025-03-24 18:37:52 +0900

    Add files via upload
    I want to go home

---
+++ 20250324.py
@@ -0,0 +1,11 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = '아'
+>>> b = '집'
+>>> c = '가고'
+>>> d = '싶다'
+>>> e = '가서'
+>>> f = '게임'
+>>> g = '하고'
+>>> print(a,b,c,d,b,e,f,g,d)
+아 집 가고 싶다 집 가서 게임 하고 싶다
